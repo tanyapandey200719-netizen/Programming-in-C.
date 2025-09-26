de006c26397c9a62bcd3b0f74426e44a97f15877
tanyapandey200719-netizen <tanyapandey200719@gmail.com>
2025-09-27 01:26:07 +0530

    Update Experiment2.2.c

--- Experiment 2/Experiment2.2.c
+++ Experiment 2/Experiment2.2.c
@@ -1,4 +1,4 @@
-//2.1:WAP a C program to Convert temperture from celsius to fahrenheit using the formula:F=(C*9/5)+32
+//2.2:WAP a C program to Convert temperture from celsius to fahrenheit using the formula:F=(C*9/5)+32
 #include<stdio.h>
 int main(){
 	printf("Name-Tanya kumari\nSAP ID-590029168\nBatch-B6\nCourse-BCA");
@@ -9,3 +9,4 @@ int main(){
 	printf("The temperature in F will be:%lf\n",F=(C*9/5));
 	return 0;
 }
+
